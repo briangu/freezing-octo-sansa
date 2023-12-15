5bee51370d81c7facb8bb6f623885c4764acce8d
Brian Guarraci <brian.guarraci@gmail.com>
2023-12-15 13:14:56 -0700

    update

---
+++ python/btree.py
@@ -0,0 +1,160 @@
+
+class Node:
+    def __init__(self, k=2, parent=None, keys=None, children=None):
+        self.parent = parent
+        self.k = k
+        self.keys = keys or []
+        self.children = children or []
+
+    def is_leaf(self):
+        return len(self.children) == 0
+
+    def is_full(self):
+        return len(self.keys) == self.k
+
+    def insert_at_leaf(self, k, v):
+        assert self.is_leaf()
+        i = 0
+        while i < len(self.keys) and self.keys[i][0] < k:
+            i += 1
+        if i < len(self.keys) and self.keys[i][0] == k:
+            self.keys[i][1].append(v)
+        else:
+            self.keys.insert(i, (k,[v]))
+
+    def insert_inner(self, k, v, left, right):
+        assert not self.is_leaf()
+        i = 0
+        while i < len(self.keys) and self.keys[i][0] < k:
+            i += 1
+        assert (i < len(self.keys) and self.keys[i][0] != k) or (i >= len(self.keys))
+        self.keys.insert(i, (k,[v]))
+        self.children.insert(i, left)
+        self.children.insert(i+1, right)
+
+
+class BTree:
+    def __init__(self, k):
+        self.k = k
+        self.root = Node(self.k)
+
+    def traverse(self):
+        if self.root:
+            self.root.traverse()
+
+    def find(self, k, v=None):
+        return self.root.find(k, v=v) if self.root else None
+
+    def split_node(self, node):
+        m_idx = len(node.keys)//2
+        m = node.keys[m_idx]
+        left = Node(self.k, parent=node.parent, keys=node.keys[:m_idx], children=node.children[:m_idx])
+        right = Node(self.k, parent=node.parent, keys=node.keys[m_idx+1:], children=node.children[m_idx+1:])
+
+        for child in left.children:
+            child.parent = left
+        for child in right.children:
+            child.parent = right
+
+        if node.parent:
+            node.parent.insert_inner(m[0], m[1], left, right)
+            if node.parent.is_full():
+                self.split_node(node.parent)
+        else:
+            self.root = Node(self.k, keys=[m], children=[left, right])
+            left.parent = self.root
+            right.parent = self.root
+
+    def insert(self, k, v):
+        current = self.root
+        if not current:
+            self.root = Node(k=self.k)
+            self.root.keys.append((k,[v]))
+
+        while not current.is_leaf():
+            chosen = None
+
+            assert len(current.keys) == len(current.children) - 1
+
+            # Determine which child to follow
+            i = 0
+            while i < len(current.keys):
+                if k < current.keys[i][0]:
+                    chosen = current.children[i]
+                    break
+                i += 1
+
+            if i >= len(current.keys):
+                chosen = current.children[-1]
+
+            assert chosen
+
+            current = chosen
+
+        # Insert the key in the leaf node
+        current.insert_at_leaf(k, v)
+
+        # Handle the case where the leaf node is full and needs splitting
+        if len(current.keys) > self.k:
+            current = self.split_node(current)
+
+    def delete(self, k, v):
+        if not self.root:
+            return
+        self.root.delete(k, v)
+
+
+import unittest
+
+class TestBTree(unittest.TestCase):
+    def test_insert_into_empty_tree(self):
+        btree = BTree(k=2)
+        btree.insert(10, 'value10')
+        self.assertIsNotNone(btree.root)
+        self.assertEqual(btree.root.keys, [(10, ['value10'])])
+
+    def test_insert_into_leaf_without_split(self):
+        btree = BTree(k=2)
+        btree.insert(10, 'value10')
+        btree.insert(5, 'value5')
+        self.assertEqual(len(btree.root.keys), 2)
+        self.assertEqual(btree.root.keys[0], (5, ['value5']))
+        self.assertEqual(btree.root.keys[1], (10, ['value10']))
+
+    def test_insert_with_leaf_split(self):
+        btree = BTree(k=2)
+        btree.insert(10, 'value10')
+        btree.insert(20, 'value20')
+        btree.insert(15, 'value15')  # This should cause a split
+
+        self.assertEqual(len(btree.root.keys), 1)
+        self.assertEqual(btree.root.keys[0][0], 15)
+        self.assertEqual(len(btree.root.children), 2)
+        # Check left child
+        self.assertEqual(btree.root.children[0].keys, [(10, ['value10'])])
+        # Check right child
+        self.assertEqual(btree.root.children[1].keys, [(20, ['value20'])])
+
+    def test_insert_with_root_split(self):
+        btree = BTree(k=2)
+        # Insert enough keys to cause a root split
+        btree.insert(10, 'value10')
+        btree.insert(20, 'value20')
+        btree.insert(30, 'value30')
+
+        self.assertIsNotNone(btree.root)
+        self.assertEqual(len(btree.root.keys), 1)
+        self.assertEqual(len(btree.root.children), 2)
+        # More assertions to verify the structure after split
+
+    def test_multiple_inserts(self):
+        btree = BTree(k=3)
+        keys = [10, 20, 5, 15, 25, 30, 3, 8]
+        for key in keys:
+            btree.insert(key, f'value{key}')
+
+        # Assertions to check the overall structure and properties of the tree
+
+
+if __name__ == "__main__":
+    unittest.main()
