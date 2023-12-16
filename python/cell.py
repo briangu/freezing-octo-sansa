dfa0b145799d5d8500e03df3a1d44ce6fc6739bc
Brian Guarraci <brian@ops5.com>
2023-12-16 09:01:10 -0700

    cleanup

--- python/cell.py
+++ python/cell.py
@@ -5,14 +5,15 @@ class Cell:
     self.deps = deps or []
     self.fn = fn
     self.cache = None
-    self.name = name or ""
+    self.name = name
 
   def __call__(self):
     if self.cache is None:
       self.cache = self.fn(*[x() for x in self.deps])
-      print(f"node: {self.name} filling cache {self.cache} from {[x.name for x in (self.deps or [])]}")
-    else:
-      print(f"node: {self.name} using cache {self.cache} from {[x.name for x in (self.deps or [])]}")
+      if self.name:
+        print(f"node: {self.name} filling cache {self.cache} from {[x.name for x in self.deps]}")
+    elif self.name:
+      print(f"node: {self.name} using cache {self.cache} from {[x.name for x in self.deps]}")
     return self.cache
 
 
