39e35ad7fa9371a390ac859097bdeaa7c38f2f0d
Brian Guarraci <brian@ops5.com>
2023-12-16 08:59:55 -0700

    add cell cache

--- python/cell.py
+++ python/cell.py
@@ -1,17 +1,27 @@
 
 class Cell:
 
-  def __init__(self, fn, deps=None):
-    self.deps = deps
+  def __init__(self, fn, deps=None, name=None):
+    self.deps = deps or []
     self.fn = fn
+    self.cache = None
+    self.name = name or ""
 
   def __call__(self):
-    return self.fn(*[x() for x in self.deps]) if self.deps else self.fn()
+    if self.cache is None:
+      self.cache = self.fn(*[x() for x in self.deps])
+      print(f"node: {self.name} filling cache {self.cache} from {[x.name for x in (self.deps or [])]}")
+    else:
+      print(f"node: {self.name} using cache {self.cache} from {[x.name for x in (self.deps or [])]}")
+    return self.cache
 
 
 if __name__ == "__main__":
-  a = Cell(lambda: 1)
-  b = Cell(lambda: 2)
-  c = Cell(lambda x,y: x+y, deps=[a,b])
+  a = Cell(lambda: 1, name="a")
+  b = Cell(lambda: 2, name="b")
+  c = Cell(lambda x,y: x+y, deps=[a,b], name="c")
   assert c() == 3
-
+  d = Cell(lambda x: x*2, deps=[c], name="d")
+  e = Cell(lambda x: x*4, deps=[c], name="e")
+  f = Cell(lambda x,y: x+y, deps=[d,e], name="f")
+  assert f() == 2*3+4*3
